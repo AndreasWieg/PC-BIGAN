185b509e5a2266f80c5e2405005b87b0068c3a78
DirtyFive <dirtyfive58@gmail.com>
2019-03-10 18:35:09 +0100

    v.0.2
    first run through

--- pcbigan.py
+++ pcbigan.py
@@ -13,9 +13,9 @@ import pandas as pd
 from plyfile import PlyData, PlyElement
 import random
 
-class PGBIGAN(object):
+class PCBIGAN(object):
 
-	def __init__(self,is_training,epoch,pointcloud_dim,checkpoint_dir, learning_rate,z_dim,batch_size,beta1,beta2,growth,number_prog):
+	def __init__(self,is_training,epoch,checkpoint_dir, learning_rate,z_dim,batch_size,beta1,beta2,pointcloud_dim):
 		self.beta1 = beta1
 		self.beta2 = beta2
 		self.learning_rate = learning_rate
@@ -26,81 +26,91 @@ class PGBIGAN(object):
 		self.epoch = epoch
 		self.checkpoint_dir = checkpoint_dir
 		self.table_fake = "table_fake"
-		self.growth = growth
-		self.number_prog = number_prog
 		self.lam_gp = 10
 		self.build_network()
-		print("start training")
+
 		print("d_optim")
 		with tf.variable_scope("adam",reuse=tf.AUTO_REUSE) as scope:
-			self.g_optim = tf.train.AdamOptimizer(self.learning_rate, beta1 = self.beta1).minimize(self.g_loss,var_list = self.vars_G)
+			print("d_optim")
+			self.g_optim = tf.train.AdamOptimizer(self.learning_rate, beta1 = self.beta1).minimize(self.g_loss,var_list = self.vars_G_E)
+			print("g_optim")
 			self.d_optim = tf.train.AdamOptimizer(self.learning_rate, beta1 = self.beta1).minimize(self.d_loss,var_list = self.vars_D)
-		print("g_optim")
+
 		self.init  = tf.global_variables_initializer()
 		self.config = tf.ConfigProto()
 		self.config.gpu_options.allow_growth = True
 
 
-	def generator(self,x,growth):
-		with tf.variable_scope("generator",reuse=tf.AUTO_REUSE) as scope:
-			for i in range(1):
-				x = tf.layers.dense(x, growth[-1]*3, activation=tf.nn.leaky_relu, name = "gen_layer_%d" % self.number_prog,reuse=tf.AUTO_REUSE)
-				x = tf.layers.dense(x, growth[-1]*3, activation=None,kernel_initializer = tf.constant_initializer(0.2), name="gen_layer_last%d" % self.number_prog,reuse=tf.AUTO_REUSE)
-			x = tf.reshape(x,[-1,growth[-1],3])
 
+	def encoder(self,y):
+		with tf.variable_scope("encoder") as scope:
+			y = tf.layers.conv1d(y,filters = 64,kernel_size=3,strides=1,padding="same",activation=tf.nn.leaky_relu,name = "enc_1")
+			y = tf.layers.conv1d(y,filters = 64,kernel_size=3,strides=1,padding="same",activation=tf.nn.leaky_relu,name = "enc_2")
+			y = tf.layers.conv1d(y,filters = 64,kernel_size=3,strides=1,padding="same",activation=tf.nn.leaky_relu, name = "enc_3")
+			y = tf.layers.conv1d(y,filters = 64,kernel_size=3,strides=1,padding="same",activation=tf.nn.leaky_relu, name = "enc_4")
+			y = tf.layers.flatten(y)
+			y = tf.layers.dense(y, 128, activation=None,kernel_initializer = tf.constant_initializer(0.2),name = "enc_output")
+		return y
+
+
+	def generator(self,x):
+		with tf.variable_scope("generator") as scope:
+			x = tf.layers.dense(x, 128, activation=tf.nn.leaky_relu,name= "gen_1")
+			x = tf.layers.dense(x, 256, activation=None,name= "gen_2")
+			x = tf.layers.dense(x, 512, activation=None,name= "gen_3")
+			x = tf.layers.dense(x, 1024, activation=None,name= "gen_4")
+			x = tf.layers.dense(x, 2048, activation=None,name= "gen_5")
+			x = tf.layers.dense(x, 3084, activation=None,name= "gen_6")
+			x = tf.reshape(x,[-1,1028,3])
 		return x
 
-	def discriminator(self,y,growth,reuse=False):
+
+	def discriminator(self,y,x,reuse=False):
 		with tf.variable_scope("discriminator") as scope:
 			if reuse:
 				scope.reuse_variables()
-			for i in range(1):
-				y = tf.layers.conv1d(y,filters = 64,kernel_size=3,strides=1,padding="same",activation=tf.nn.leaky_relu,name = "conv_%d" % (self.number_prog),reuse=tf.AUTO_REUSE)
-			y = tf.layers.dense(y, 128,activation=tf.nn.leaky_relu, name="conv_1last",reuse=tf.AUTO_REUSE)
-			y = tf.layers.dense(y, 1, activation=None,name="conv_last",reuse=tf.AUTO_REUSE)
-
+			y = tf.layers.conv1d(y,filters = 64,kernel_size=3,strides=1,padding="same",activation=tf.nn.leaky_relu)
+			y = tf.layers.dense(y, 128,activation=tf.nn.leaky_relu, name="conv_1last")
+			y = tf.layers.dense(y, 1, activation=tf.nn.softmax,name="conv_last")
 		return y
 
 	def build_network(self):
 		eps = 1e-12
-		self.input = tf.placeholder(tf.float32, [None,self.growth[-1],3], name="real_pointcloud_data")
+		self.input = tf.placeholder(tf.float32, [None,self.pointcloud_dim,3], name="real_pointcloud_data")
 		self.z = tf.placeholder(tf.float32,[None,self.z_dim], name ="noice")
-		self.Gen = self.generator(self.z,self.growth)
-		self.Dis_real = self.discriminator(self.input,self.growth,reuse = False)
-		self.Dis_fake = self.discriminator(self.Gen,self.growth,reuse = True)
+		self.Gen = self.generator(self.z)
+		self.Enc = self.encoder(self.input)
+		self.Dis_encoder = self.discriminator(self.input,self.Enc,reuse = False)
+		self.Dis_generator = self.discriminator(self.Gen,self.z,reuse = True)
 
 		#Tensorboard variables
-		self.d_sum_real = tf.summary.histogram("d_real", self.Dis_real)
-		self.d_sum_fake = tf.summary.histogram("d_fake", self.Dis_fake)
+		self.d_sum_real = tf.summary.histogram("d_real", self.Dis_encoder)
+		self.d_sum_fake = tf.summary.histogram("d_fake", self.Dis_generator)
+		self.Enc_sum = tf.summary.histogram("Enc", self.Enc)
 		self.G_sum = tf.summary.histogram("G",self.Gen)
 		self.z_sum = tf.summary.histogram("z_input",self.z)
 
 		#Wassersteinmetrik
-		self.d_loss = tf.reduce_mean(self.Dis_fake - self.Dis_real)
-		self.g_loss = -tf.reduce_mean(self.Dis_fake)
+		#self.d_loss = tf.reduce_mean(self.Dis_encoder - self.Dis_generator)
+		#self.g_loss = -tf.reduce_mean(self.Dis_generator - (1 - self.Dis_encoder))
 
-		# gradient penalty
-		self.dif = self.Gen - self.input
-		self.alpha = tf.random_uniform(shape=[self.batch_size, 1, 1, 1], minval=0., maxval=1.)
-		self.interpoltate = self.input + (self.alpha * self.dif)
-		discre_logits = self.discriminator(self.interpolate,self.growth,reuse = True)
-		gradients = tf.gradients(discri_logits, [interpolates])[0]
 
-		slopes = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1, 2, 3]))
-        self.gradient_penalty = tf.reduce_mean((slopes - 1.) ** 2)
+		#Vanilla BI-GAN Loss
+		self.d_loss = tf.reduce_mean(-tf.log(self.Dis_encoder) - tf.log(1. - self.Dis_generator))
+		self.g_loss = tf.reduce_mean(-tf.log(self.Dis_generator) - tf.log(1 - self.Dis_encoder))
 
-		self.d_loss += self.lam_gp * self.gradient_penalty
-
-		#Vanilla GAN
-		#self.d_loss = tf.reduce_mean(-tf.log(self.Dis_real) - tf.log(1. - self.Dis_fake))
-		#self.g_loss = tf.reduce_mean(-tf.log(self.Dis_fake))
 		tf.summary.scalar('self.g_loss', self.g_loss )
 		tf.summary.scalar('self.d_loss', self.d_loss )
 
+		#collect generator and encoder variables
 		t_vars = tf.trainable_variables()
-		self.vars_D = [var for var in t_vars if 'conv' in var.name]
+		self.vars_G_E = [var for var in t_vars if 'gen' or "enc" in var.name]
+
+		#self.vars_G = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
+		#self.vars_E = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='encoder')
 		self.vars_D = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
-		self.vars_G = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
+
+
 		self.saver = tf.train.Saver()
 
 		#Tensorboard variables
@@ -112,13 +122,12 @@ class PGBIGAN(object):
 		with tf.Session(config = self.config) as sess:
 			#imported_meta = tf.train.import_meta_graph("C:/Users/Andreas/Desktop/punktwolkenplot/pointgan/checkpoint/model.ckpt-4.meta")
 			#imported_meta.restore(sess, "C:/Users/Andreas/Desktop/punktwolkenplot/pointgan/checkpoint/model.ckpt-4")
-
 			train_writer = tf.summary.FileWriter("./logs",sess.graph)
 			merged = tf.summary.merge_all()
 			#test_writer = tf.summary.FileWriter("C:/Users/Andreas/Desktop/punktwolkenplot/pointgan/")
 			self.counter = 1
 			sess.run(self.init)
-			self.training_data = load_data_table(self.growth[-1],len(self.growth))
+			self.training_data = load_data_table(self.pointcloud_dim,4)
 			k = (len(self.training_data) // self.batch_size)
 			self.start_time = time.time()
 			loss_g_val,loss_d_val = 0, 0
@@ -131,13 +140,12 @@ class PGBIGAN(object):
 				epoch_loss_g = 0.
 				print("data shuffeld")
 				self.training_data = shuffle_data(self.training_data)
-
 				for i in range(0,k):
 					self.batch_z = np.random.uniform(0, 0.2, [self.batch_size, self.z_dim])
 					self.batch = self.training_data[i*self.batch_size:(i+1)*self.batch_size]
-					_, loss_d_val,loss_d = sess.run([self.d_optim,self.d_loss,self.summary_d_loss],feed_dict={self.input: self.batch,self.z: self.batch_z})
+					_, loss_d_val,loss_d = sess.run([self.d_optim,self.d_loss,self.summary_d_loss],feed_dict={self.input: self.batch, self.z: self.batch_z})
 					train_writer.add_summary(loss_d,self.counter)
-					_, loss_g_val,loss_g = sess.run([self.g_optim,self.g_loss,self.summary_g_loss],feed_dict={self.z: self.batch_z})
+					_, loss_g_val,loss_g = sess.run([self.g_optim,self.g_loss,self.summary_g_loss],feed_dict={self.z: self.batch_z,self.input: self.batch})
 					train_writer.add_summary(loss_g,self.counter)
 					self.counter=self.counter + 1
 					epoch_loss_d += loss_d_val
@@ -148,6 +156,8 @@ class PGBIGAN(object):
 				print("Loss of G: %f" % epoch_loss_g)
 				print("Epoch%d" %(e))
 
+
+				'''
 				if e % 100 == 0:
 					#save_path = self.saver.save(sess,"C:/Users/Andreas/Desktop/punktwolkenplot/pointgan/checkpoint/model.ckpt",global_step=e)
 					#print("model saved: %s" %save_path)
@@ -155,5 +165,5 @@ class PGBIGAN(object):
 					test_table = sess.run([self.Gen], feed_dict={self.z: self.gen_noise})
 					save_pointcloud(test_table,self.number_prog,self.table_fake, self.growth[-1])
 					print("created fake_test_leaf")
-
+				'''
 			print("training finished")
